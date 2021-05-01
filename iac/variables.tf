278329243e9f0db3bbcb103ec5a8ef7af8455de6
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-01 13:11:48 +0530

    iac: add resource group

---
+++ iac/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  description = "Resource group to create the resources in"
+  type = string
+}
+
+variable "location" {
+  description = "Location to create the resources in"
+  type = string
+}
